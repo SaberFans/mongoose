6737344cd26820d0fc956f39fa12245c2dce2ca9
ivyfans <wtyang926@gmail.com>
2018-04-05 09:45:55 +0100

    start socket server on 8000

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require('express'),
   http = require('http'),
   app = module.exports.app = express(),
-  // set up the socket http handler
-  server = http.createServer(app),
-  io = require('socket.io').listen(server),
-  // socket is listening on 8000
-  server.listen(8000),
-
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   iotModel = require('./api/models/iotModel'), //created model loading here
   housePrModel = require('./api/models/houseprModel'),
-  
   bodyParser = require('body-parser');
 
+// set up the socket http handler
+var server = http.createServer(app),
+  io = require('socket.io').listen(server);
+// socket is listening on 8000
+server.listen(8000);
+  
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -29,6 +28,8 @@ routes(app); //register the route
 
 
 app.listen(port);
+
+
 
 console.log('IoT data store API server started on: ' + port);
 
